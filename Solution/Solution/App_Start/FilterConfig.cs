1efd7cec7f9cdee4cfb2651279c2f68ffbe7cbd9
Fernando Mathews <fmathews28@outlook.com>
2015-12-22 14:28:41 -0500

    First Commit : From local repo

---
+++ Solution/Solution/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Solution {
+   public class FilterConfig {
+      public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+         filters.Add(new HandleErrorAttribute( ));
+      }
+   }
+}
